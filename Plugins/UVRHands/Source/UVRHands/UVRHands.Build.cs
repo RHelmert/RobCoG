cf8013932298cc1d0bbe31768298a95f5a0eb660
Rhelmert <rhelmert2@gmail.com>
2023-10-26 13:34:52 +0200

    implement working and configurable Manus rating gesture

--- Plugins/UVRHands/Source/UVRHands/UVRHands.Build.cs
+++ Plugins/UVRHands/Source/UVRHands/UVRHands.Build.cs
@@ -1,5 +1,6 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
+using System.IO;
 using UnrealBuildTool;
 
 public class UVRHands : ModuleRules
@@ -7,33 +8,33 @@ public class UVRHands : ModuleRules
 	public UVRHands(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
-		
+
 		PublicIncludePaths.AddRange(
 			new string[] {
 				// ... add public include paths required here ...
 			}
 			);
-				
-		
+
+
 		PrivateIncludePaths.AddRange(
 			new string[] {
 				// ... add other private include paths required here ...
 			}
 			);
-			
-		
+
+
 		PublicDependencyModuleNames.AddRange(
 			new string[]
 			{
 				"Core",
 				"Manus",
-                "RHI",
-                "RenderCore",
+				"RHI",
+				"RenderCore",
 				// ... add other public dependencies that you statically link with here ...
 			}
 			);
-			
-		
+
+
 		PrivateDependencyModuleNames.AddRange(
 			new string[]
 			{
@@ -44,13 +45,15 @@ public class UVRHands : ModuleRules
 				// ... add private dependencies that you statically link with here ...	
 			}
 			);
-		
-		
+
+
 		DynamicallyLoadedModuleNames.AddRange(
 			new string[]
 			{
 				// ... add any modules that your module loads dynamically here ...
 			}
 			);
-	}
+
+    }
+
 }
